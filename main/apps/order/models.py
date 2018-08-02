50ff4fa57eb385b03e9a9c8cee01388d82471f9a
paulino <psantosiii@yahoo.com>
2018-08-02 10:09:13 -0400

    day4

--- main/apps/order/models.py
+++ main/apps/order/models.py
@@ -37,8 +37,6 @@ class Order(models.Model):
     order_id            = models.CharField(max_length=120, blank=True) 
     shipping_address    = models.ForeignKey(User, related_name="shipping_address",null=True, blank=True)
     billing_address     = models.ForeignKey(User, related_name="billing_address", null=True, blank=True)
-    shipping_address_final    = models.TextField(blank=True, null=True)
-    billing_address_final     = models.TextField(blank=True, null=True)
     # cart                = models.ForeignKey(Cart)
     status              = models.CharField(max_length=120, default='created', choices=ORDER_STATUS_CHOICES)
     shipping_total      = models.DecimalField(default=5.99, max_digits=100, decimal_places=2)
